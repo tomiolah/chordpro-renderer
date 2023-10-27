dbe9a962ed237b16fa3435311e9a31c85e739230
Tomi Olah <tomiolah1998@gmail.com>
2023-10-27 19:41:16 +0300

    feat: add test data

---
+++ src/data.ts
@@ -0,0 +1,49 @@
+export const TEST_SONG = `{c: Verse 1}
+[Dm] Sokszor úgy érzem [C] megvagyok, bár alig [B]létezem.
+[Dm] Telnek napjaim, [C] s életem nélküled [B]fényezem.
+[Dm] Folyton elbukok, [C] de segít - [F]séged mindig [B]kérhetem.
+[Dm] Köszönöm neked, [C] hogy rád bíz - [F]hatom bűnös [B]életem.
+
+{c: Chorus}
+[F] Tehozzád fu - [C]tok, Neked hódo - [Dm]lok,
+Uram [C] átadom [B]mindenem.
+[F] Csak nálad van re - [C]mény, igazság és [Dm]fény.
+Kérlek [C]őrizd meg [B]életem.
+
+{c: Verse 2}
+[Dm] Ha összetört szívem, [C] nálad begyógyulhat [B]a sebem.
+[Dm] Minden egyes nap, [C] Uram, jelenléted [B]éhezem.
+[Dm] A lábaid elé, [F] minden nehézségem [B]leteszem.
+[Dm] Ha egyedül volnék, [C] tudom, Te [F]akkor is fogod [B]két kezem.
+
+{c: Chorus}
+[F] Tehozzád fu - [C]tok, Neked hódo - [Dm]lok,
+Uram [C] átadom [B]mindenem.
+[F] Csak nálad van re - [C]mény, igazság és [Dm]fény.
+Kérlek [C]őrizd meg [B]életem.
+
+[Dm]  [C] [B]
+
+{c: Bridge}
+[Dm]Tisztíts [C]meg, Tisztíts [B]meg.
+Uram, [Dm]tisztíts [C]meg, Tisztíts [B]meg.
+[Dm]Tisztíts [C]meg, Tisztíts [B]meg.
+Uram, [Dm]tisztíts [C]meg, [F/A]Tisztíts [B]meg.
+
+{c: Chorus}
+[F] Tehozzád fu - [C]tok, Neked hódo - [Dm]lok,
+Uram [C] átadom [B]mindenem.
+[F] Csak nálad van re - [C]mény, igazság és [Dm]fény. [C] [B]
+
+{c: Chorus}
+[F] Tehozzád fu - [C]tok, Neked hódo - [Dm]lok,
+Uram [C] átadom [B]mindenem.
+[F] Csak nálad van re - [C]mény, igazság és [Dm]fény.
+Kérlek [C]őrizd meg [B]életem.
+[Dm] Kérlek [C]őrizd meg [B]életem.
+[Dm] Uram, [C]őrizd meg [B]életem.
+
+{c: Outro}
+[F]  [F/E]    [F/Eb]     [C#]   [F/Eb]
+[F]  [F/E]    [F/Eb]     [C#]
+`;
